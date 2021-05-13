9fc62c7ca11b6acc573483f896bc2ff3f76032f4
udykas <udykas@gmail.com>
2021-05-12 21:31:04 -0400

    Now displays the SQL and JSON output on screen

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,25 @@
-import QueryBuilder from 'react-querybuilder';
+import { useState } from 'react';
+import QueryBuilder, { formatQuery, RuleGroupType } from 'react-querybuilder';
+
 import './App.scss';
 
 function App() {
+  const [query, setQuery] = useState<RuleGroupType>({
+    id: "root",
+    combinator: "and",
+    rules: []
+  });
+
   return (
-    <QueryBuilder
-      fields={[{ name: "test", label: "Test" }]}
-      onQueryChange={(q) => console.log(q)}
-    />
+    <>
+      <QueryBuilder
+        fields={[{ name: "test", label: "Test" }]}
+        query={query}
+        onQueryChange={(q) => setQuery(q)}
+      />
+      <pre>{formatQuery(query, "sql")}</pre>
+      <pre>{formatQuery(query, "json")}</pre>
+    </>
   );
 }
 
